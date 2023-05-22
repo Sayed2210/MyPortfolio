55690134b32929d9f3ac0040e8024a5c8b34aa45
Elsayed Hassan <eh600039@gmail.com>
2023-05-22 15:34:41 +0300

    finish the portfolio

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// dtxlvjhvgvmumwms
+const btn= document.querySelector("#submit")
+btn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var text = document.getElementById('text').value;
+    var subject = document.getElementById('subject').value;
+    var body= `name is ${name} 
+                email : ${email}
+                title: ${text}
+                phone: ${number}
+                subject ${subject}`
+    Email.send({
+        host: 'smtp.elasticemail.com',
+        Username: 'eh600039@gmail.com',
+        Password: 'dtxlvjhvgvmumwms',
+        To: 'eh600039@gmail.com',
+        From: email,
+        Subject: text,
+        Body: body
+    }).then(
+        alert("Send ")
+    )
+
+
+
+
+
+})
